4eb72e1dba26696b6a26742ddd9efe6179baefe1
monisha-a-14 <moniishabista@gmail.com>
2026-08-19 11:16:38 +0530

    Time: 1 ms (97.94%), Space: 42.7 MB (25.86%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int addDigits(int n) {
+       
+       while(n>=10){
+        int sum=0;
+        while(n>0){
+            sum=sum+n%10;
+            n=n/10;
+        }
+        n=sum;
+       } 
+       return n;
+    }
+}
