941866726bfe4580b8fdd678050005ccb567c741
monisha-a-14 <moniishabista@gmail.com>
2026-08-27 13:26:14 +0530

    Time: 8 ms (17%), Space: 47.9 MB (12.2%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int low=0,high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==mid){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+    }
+}
